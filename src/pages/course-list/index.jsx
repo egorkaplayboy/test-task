c8f43318e613abde499e04a749567e8e39e65fa4
egorkaplayboy <bel04ka228@mail.ru>
2023-10-07 19:31:40 +0300

    Added wrapper and SEO for course-list

--- src/pages/course-list/index.jsx
+++ src/pages/course-list/index.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import CourseList from "../../components/course-list/index"
+import React from "react";
+import CourseList from "../../components/course-list/index";
+import SEO from "@/src/common/seo";
+import WrapperFour from "@/src/layout/wrapper-4";
 
-const Courses = ({courseListData}) => {
+const Courses = ({ courseListData }) => {
   return (
-    <CourseList courseListData={courseListData}/>
-  )
-}
+    <WrapperFour>
+      <SEO pageTitle={"Course List"}/>
+      <CourseList courseListData={courseListData} />
+    </WrapperFour>
+  );
+};
 
-export default Courses
-
+export default Courses;
 
 export async function getServerSideProps() {
-  const courseListData = await fetch("http://localhost:3000/api/course-list-data/").then(
-    (res) => res.json()
-  );
-  
+  const courseListData = await fetch(
+    "http://localhost:3000/api/course-list-data/"
+  ).then((res) => res.json());
 
   return {
     props: {
-      courseListData
+      courseListData,
     },
   };
 }
